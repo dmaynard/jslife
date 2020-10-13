6f64a410101af7ffa57bb5300300c3ca1f5dea9c
dmaynard <davidsmaynard@gmail.com>
2020-10-13 12:46:27 -0700

    random init with Go button and single step

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-import * as wasm from "hello-wasm-pack";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const gob = document.getElementById("go-button");
+const universe = Universe.new();
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+  requestAnimationFrame(renderLoop);
+};
+// function startAnimation() {
+//   requestAnimationFrame(renderLoop);
+// }
+pre.addEventListener("mousedown", function () {
+  universe.tick();
+  pre.textContent = universe.render();
+});
+gob.addEventListener("mousedown", function () {
+  requestAnimationFrame(renderLoop);
+});
+// universe.make_spaceship();
+// universe.make_spaceship();
+pre.textContent = universe.render();
+
+// pre.dblclick(requestAnimationFrame(renderLoop));
+//
