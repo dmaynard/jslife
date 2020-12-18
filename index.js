cbaacb127e42a55b9414c11f65516bf703a35ddd
dmaynard <davidsmaynard@gmail.com>
2020-12-17 17:40:05 -0800

    fps and bug fixes
    fixed bug in cell getCellSize and added fps timer

--- index.js
+++ index.js
@@ -148,6 +148,7 @@ canvas.addEventListener("click", (event) => {
   const row = Math.min(Math.floor(canvasTop / (CellSize + 1)), height - 1);
   const col = Math.min(Math.floor(canvasLeft / (CellSize + 1)), width - 1);
 
+  console.log(" row: " + row + " col " + col);
   universe.toggle_cell(row, col);
 
   drawGrid();
@@ -169,9 +170,7 @@ const drawCells = () => {
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
-
       ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
-
       ctx.fillRect(
         col * (CellSize + 1) + 1,
         row * (CellSize + 1) + 1,
@@ -187,20 +186,22 @@ const drawCells = () => {
 console.log("Executing top level script code");
 
 const getCellSize = () => {
-  let sizeW = Math.floor((canvas.width - 2 * MARGIN) / universe.width());
+  let sizeW = Math.floor(canvas.width / universe.width());
   let sizeH = Math.floor(canvas.height / universe.height());
-  return Math.min(sizeW, sizeH);
+  return Math.min(sizeW, sizeH) - 1; // -1 to allow for the gridding between cells
 };
 
 function resizeCanvas() {
   console.log(" Resize (" + window.innerWidth + "," + window.innerHeight + ")");
-  let minDim = Math.min(window.innerWidth, window.innerHeight - 100);
+  let minDim = Math.min(
+    window.innerWidth - 2 * MARGIN,
+    window.innerHeight - 5 * MARGIN
+  );
   canvas.width = minDim;
   canvas.height = minDim;
+  console.log(" Canvas: (" + canvas.width + "," + canvas.height + ")");
   if (fixedUniverseSize) {
     CellSize = getCellSize();
-    // canvas.width = (CellSize + 1) * universe.width() + 2 * MARGIN;
-    // canvas.height = (CellSize + 1) * universe.height();
     drawGrid();
     drawCells();
   } else {
@@ -221,6 +222,7 @@ function resizeCanvas() {
 const fps = new (class {
   constructor() {
     this.fps = document.getElementById("fps");
+    this.fps2 = document.getElementById("fps2");
     this.frames = [];
     this.lastFrameTimeStamp = performance.now();
   }
@@ -251,12 +253,8 @@ const fps = new (class {
     let mean = sum / this.frames.length;
 
     // Render the statistics.
-    this.fps.textContent = `
-Frames per Second:
-         latest = ${Math.round(fps)}
-avg of last 100 = ${Math.round(mean)}
-min of last 100 = ${Math.round(min)}
-max of last 100 = ${Math.round(max)}
+    this.fps2.textContent = `
+Frames per Second: ${Math.round(mean)}
 `.trim();
   }
 })();
